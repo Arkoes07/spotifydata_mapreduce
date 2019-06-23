80ef0541d3794365f3ecefa15fdcd10cedd8a2ac
Arkoes07 <m.alwi@ui.ac.id>
2019-06-23 20:29:29 +0700

    mapred program

---
+++ src/main/java/ArtistMapper.java
@@ -0,0 +1,19 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class ArtistMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        // Split CSV
+        String row = value.toString();
+        String[] rowData = row.split(",");
+
+        if(rowData[1] != null){
+           String artistName = rowData[1].replaceAll(" ","_");
+           context.write(new Text(artistName), new IntWritable(1));
+        }
+    }
+}
